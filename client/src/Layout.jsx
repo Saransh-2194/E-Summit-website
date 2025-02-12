6d668238bd49efc173e161961df3e4e3338abcb1
Saransh <saranshagarwal2004@gmail.com>
2025-02-13 01:56:23 +0530

    try

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import { Outlet } from 'react-router-dom';
-import ScrollToTop from './components/ScrollToTop';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
 
 function Layout() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-        <ScrollToTop /> 
-        <Outlet />
+      <ScrollToTop />
+      <Outlet />
       <Footer />
     </div>
   );
