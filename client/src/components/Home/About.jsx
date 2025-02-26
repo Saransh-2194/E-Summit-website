0faa57c11054698c3cf9c5d255271986b139ff86
Saransh <saranshagarwal2004@gmail.com>
2025-02-26 22:17:57 +0530

    v1.1.21 final

--- client/src/components/Home/About.jsx
+++ client/src/components/Home/About.jsx
@@ -9,9 +9,13 @@ export default function AboutUs() {
 
   const { ref: firstSectionRef, inView: firstSectionInView } = useInView({ triggerOnce: true });
   const { ref: secondSectionRef, inView: secondSectionInView } = useInView({ triggerOnce: true });
+  const { ref: thirdSectionRef, inView: thirdSectionInView } = useInView({ triggerOnce: true });
+  const { ref: newSectionRef, inView: newSectionInView } = useInView({ triggerOnce: true });
 
   const firstSectionControls = useAnimation();
   const secondSectionControls = useAnimation();
+  const thirdSectionControls = useAnimation();
+  const newSectionControls = useAnimation();
 
   useEffect(() => {
     if (firstSectionInView) {
@@ -33,6 +37,17 @@ export default function AboutUs() {
     }
   }, [secondSectionInView, secondSectionControls]);
 
+  useEffect(() => {
+    if (thirdSectionInView) {
+      thirdSectionControls.start({
+        opacity: 1,
+        x: 0,
+        transition: { duration: 2.5 },
+      });
+    }
+  }, [thirdSectionInView, thirdSectionControls]);
+
+
   return (
     <div className="text-white min-h-screen px-6 sm:px-12 md:px-20 lg:px-32 py-4">
       {/* Header */}
@@ -40,11 +55,60 @@ export default function AboutUs() {
         <img src="/about.png" alt="About Us" className="h-15 md:h-28 lg:h-25" />
       </div>
 
-      {/* First Section: TIEDC */}
+      {/* FIrst Section: JUIT */}
       <motion.div
         ref={firstSectionRef}
         initial={{ opacity: 0, x: 100 }}
         animate={firstSectionControls}
+        className="flex flex-col-reverse lg:flex-row items-center gap-12 mb-20"
+      >
+        {/* Text Content */}
+        <div className="relative w-full lg:w-3/3 text-center lg:text-left">
+          {/* Background Image */}
+          <motion.div
+            className="absolute inset-0"
+            style={{
+              backgroundImage: "url('/torn-page.png')",
+              backgroundSize: "125% 125%", // Increase the size of the background image
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+            }}
+            initial={{ opacity: 0, x: 100 }}
+            animate={firstSectionControls}
+          ></motion.div>
+          <motion.div
+            className="relative z-10 p-10 max-w-full"
+            style={cardTextStyle}
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: 1.5, duration: 1 }} // Add delay to text animation
+          >
+            <h2 className="text-black text-3xl font-bold mb-4 text-center lg:text-left ml-4 mt-8 lg:mt-0" style={cardTextStyle}>
+              JUIT
+            </h2>
+            <p className="text-black mt-4 leading-relaxed ml-4 mr-4 mb-10 lg:mb-1">
+            Jaypee University of Information Technology (JUIT) is a premier institute located in Waknaghat, Himachal Pradesh, dedicated to academic excellence and research in engineering, technology, and applied sciences. Established in 2002 under the aegis of the Jaypee Group, JUIT offers a dynamic learning environment with state-of-the-art infrastructure, modern laboratories, and a research-driven curriculum.
+            With a strong focus on innovation and entrepreneurship, JUIT fosters industry collaborations and hands-on learning experiences, preparing students for global challenges. The university is known for its vibrant campus life, technical and cultural events, and a highly qualified faculty committed to nurturing future leaders.
+            <a href="https://www.juit.ac.in/tiedc-cell" className="text-white mb-4 inline-block hover:underline ml-3">
+              Explore JUIT
+            </a>
+            </p>
+          </motion.div>
+        </div>
+
+        {/* Image */}
+        <div className="w-full lg:w-4/5">
+          <a href="https://www.juit.ac.in" target="_blank" rel="noopener noreferrer">
+            <img src="/juit.jpg" alt="JUIT" className="rounded-xl shadow-lg w-full" />
+          </a>
+        </div>
+      </motion.div>
+
+      {/* Second Section: TIEDC */}
+      <motion.div
+        ref={secondSectionRef}
+        initial={{ opacity: 0, x: 100 }}
+        animate={secondSectionControls}
         className="flex flex-col lg:flex-row items-center gap-12 mb-20"
       >
         {/* Image */}
@@ -67,7 +131,7 @@ export default function AboutUs() {
               fontFamily: "'Comic Neue', cursive",
             }}
             initial={{ opacity: 0, x: 100 }}
-            animate={firstSectionControls}
+            animate={secondSectionControls}
           ></motion.div>
           <motion.div
             className="relative z-10 p-10 max-w-full"
@@ -76,22 +140,19 @@ export default function AboutUs() {
             animate={{ opacity: 1, x: 0 }}
             transition={{ delay: 1.5, duration: 1 }} // Add delay to text animation
           >
-            <h2 className="text-black text-3xl font-bold lg:text-left ml-2 mt-10 lg:mt-0" style= {cardTextStyle}>TIEDC-JUIT</h2>
-            <p className="text-black mt-4 leading-relaxed ml-4 mr-1">
+            <h2 className="text-black text-3xl font-bold lg:text-left ml-2 mt-10 lg:mt-0" style= {cardTextStyle}>TIEDC</h2>
+            <p className="text-black mt-4 leading-relaxed ml-4 mr-1 mb-4">
               <strong>The Technology Incubation and Entrepreneurship Development Cell (TIEDC)</strong> is an initiative to foster innovation, creativity, and entrepreneurship within academic and business communities. Established in 2017 with support from the <strong>Department of Industries, Govt. of Himachal Pradesh</strong>, we aim to create a dynamic platform for individuals with innovative ideas and visions. By bridging the gap between the corporate world and aspiring entrepreneurs, we nurture an entrepreneurial ecosystem where ideas flourish and opportunities thrive, inspiring the next generation of entrepreneurs and driving positive change in our community.
             </p>
-            <a href="https://www.juit.ac.in/tiedc-cell" className="text-white mb-4 inline-block hover:underline ml-3">
-              Explore JUIT
-            </a>
           </motion.div>
         </div>
       </motion.div>
 
-      {/* Second Section: E-Summit */}
+      {/* Third Section: E-Summit */}
       <motion.div
-        ref={secondSectionRef}
+        ref={thirdSectionRef}
         initial={{ opacity: 0, x: 100 }}
-        animate={secondSectionControls}
+        animate={thirdSectionControls}
         className="flex flex-col-reverse lg:flex-row items-center gap-12"
       >
         {/* Text Content */}
@@ -106,7 +167,7 @@ export default function AboutUs() {
               backgroundRepeat: "no-repeat",
             }}
             initial={{ opacity: 0, x: 100 }}
-            animate={secondSectionControls}
+            animate={thirdSectionControls}
           ></motion.div>
           <motion.div
             className="relative z-10 p-10 max-w-full"
